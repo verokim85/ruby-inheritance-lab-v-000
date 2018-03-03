107f13c6512088d5867bcd609a74795402a8fb1a
Veronica Kim <vero.a.kim@gmail.com>
2018-03-03 04:03:03 +0000

    automatically backed up by learn

--- lib/teacher.rb
+++ lib/teacher.rb
@@ -1,20 +1,20 @@
 class Teacher < User
 
-  attr_accessor :knowledge
-
   KNOWLEDGE = ["a String is a type of data in Ruby", "programming is hard, but it's worth it", "javascript async web request", "Ruby method call definition", "object oriented dog cat class instance", "class method class variable instance method instance variable", "programming computers hacking learning terminal", "bash Ruby rvm update certs"]
 
 
   def initialize
-    @knowledge = KNOWLEDGE
+    @knowledge = []
   end
 
   def teach
-    knowledge
+    KNOWLEDGE.map do |word|
+    @knowledge << word
+    end
   end
 
-  # def knowledge
-  #   @knowledge
-  # end
+  def knowledge
+    @knowledge
+  end
 
 end

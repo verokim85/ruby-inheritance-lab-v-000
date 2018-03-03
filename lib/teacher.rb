6b76cbc15dbb9622a199aec7a9a0cf2c9056664d
Veronica Kim <vero.a.kim@gmail.com>
2018-03-03 04:26:03 +0000

    automatically backed up by learn

--- lib/teacher.rb
+++ lib/teacher.rb
@@ -1,17 +1,18 @@
 class Teacher < User
-@@all = {}
+@@all = []
 
     KNOWLEDGE = ["a String is a type of data in Ruby", "programming is hard, but it's worth it", "javascript async web request", "Ruby method call definition", "object oriented dog cat class instance", "class method class variable instance method instance variable", "programming computers hacking learning terminal", "bash Ruby rvm update certs"]
 
 
     def initialize
-      @knowledge = knowledge
+      @@all = all
+      @@all << self 
     end
 
 
     def teach
-    print @knowledge = KNOWLEDGE
-      # puts @knowledge
+      @@all << KNOWLEDGE
+      puts all 
     end
 
     def knowledge

47880bc625c13c7128a477e64252a4b25045db5c
Veronica Kim <vero.a.kim@gmail.com>
2018-03-03 04:06:00 +0000

    automatically backed up by learn

--- lib/teacher.rb
+++ lib/teacher.rb
@@ -5,19 +5,19 @@ class Teacher < User
   end
 
 
-def teach 
-  
-  KNOWLEDGE = ["a String is a type of data in Ruby", "programming is hard, but it's worth it", "javascript async web request", "Ruby method call definition", "object oriented dog cat class instance", "class method class variable instance method instance variable", "programming computers hacking learning terminal", "bash Ruby rvm update certs"]
+  def teach
+    KNOWLEDGE = ["a String is a type of data in Ruby", "programming is hard, but it's worth it", "javascript async web request", "Ruby method call definition", "object oriented dog cat class instance", "class method class variable instance method instance variable", "programming computers hacking learning terminal", "bash Ruby rvm update certs"]
 
+    KNOWLEDGE.join
+    @knowledge << KNOWLEDGE
 
-  
   # def teach
   #   return KNOWLEDGE
   #   # KNOWLEDGE.map do |word|
   #   #   word
   #   # end
   # end
-  # 
+  #
   # def knowledge
   #   @knowledge
   # end

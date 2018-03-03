3683bb0b3c8c35856d4ff7df3ed611001ef78a8c
Veronica Kim <vero.a.kim@gmail.com>
2018-03-03 04:18:12 +0000

    automatically backed up by learn

--- lib/teacher.rb
+++ lib/teacher.rb
@@ -5,7 +5,7 @@ attr_accessor :student
     KNOWLEDGE = ["a String is a type of data in Ruby", "programming is hard, but it's worth it", "javascript async web request", "Ruby method call definition", "object oriented dog cat class instance", "class method class variable instance method instance variable", "programming computers hacking learning terminal", "bash Ruby rvm update certs"]
 
     def teach
-      studetn.learn(KNOWLEDGE)
+      student.learn(KNOWLEDGE)
     end
 
 end
